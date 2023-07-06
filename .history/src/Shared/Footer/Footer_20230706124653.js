bd50ccdeb5c70a9c0f75c111b9b8cf15c80add02
wahid-ahmed <wahidahmedshanto@gmail.com>
2023-07-06 22:56:37 +0600

    make appointment and a simple ticky margin

---
+++ .history/src/Shared/Footer/Footer_20230706124653.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import footer from '../../assets/images/footer.png';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer
+            style={
+                {
+                    background: `url(${footer})`,
+                    backgroundSize: 'cover',
+
+                }
+            }
+            className="p-10 mt-20">
+            <div className='footer justify-around'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Branding</Link>
+                    <Link to='/' className="link link-hover">Design</Link>
+                    <Link to='/' className="link link-hover">Marketing</Link>
+                    <Link to='/' className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
